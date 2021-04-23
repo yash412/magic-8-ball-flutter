9122a0bf655a7318368b520cc6336d6795eee14f
Yash Choudhari <yashchoudhari.ysc@gmail.com>
2021-04-23 18:38:33 +0530

    Completed Practice app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,43 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int ball = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue.shade700,
+          title: Center(
+            child: Text('Magic App'),
+          ),
+        ),
+        body: Container(
+          child: Center(
+            // ignore: deprecated_member_use
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  ball = Random().nextInt(5) + 1;
+                });
+              },
+              child: Image.asset('images/ball$ball.png'),
+            ),
+          ),
+        ),
       ),
     );
+  }
+}
